9a41191174c2c2fd72eb710a6c3a080e3115f472
kolena <kolena@avast.com>
2018-05-28 09:36:38 +0200

    ReadingStorClient without authentication

--- src/main/scala/com/avast/clients/stor/StorClient.scala
+++ src/main/scala/com/avast/clients/stor/StorClient.scala
@@ -11,6 +11,7 @@ import monix.execution.Scheduler
 import org.http4s.Uri
 import org.http4s.client.Client
 import org.http4s.client.blaze.{BlazeClientConfig, Http1Client}
+import pureconfig.error.{ConfigReaderException, ConfigReaderFailure, ConfigReaderFailures, ConfigValueLocation}
 import pureconfig.modules.http4s.uriReader
 import pureconfig.{CamelCase, ConfigFieldMapping, ProductHint}
 
@@ -18,11 +19,13 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.language.higherKinds
 
-trait StorClient[F[_]] {
+trait ReadingStorClient[F[_]] {
   def head(sha256: Sha256): F[Either[StorException, HeadResult]]
 
   def get(sha256: Sha256, dest: File = File.newTemporaryFile(prefix = "stor")): F[Either[StorException, GetResult]]
+}
 
+trait StorClient[F[_]] extends ReadingStorClient[F] {
   def post(sha256: Sha256)(is: InputStream): F[Either[StorException, PostResult]]
 }
 
@@ -39,14 +42,36 @@ object StorClient {
     val conf = pureconfig.loadConfigOrThrow[StorClientConfiguration](config.withFallback(DefaultConfig))
     val httpClient: Client[Task] = Await.result(Http1Client[Task](conf.toBlazeConfig.copy(executionContext = sch)).runAsync, Duration.Inf)
 
+    val auth = conf.auth.getOrElse {
+      throw new ConfigReaderException[StorClientConfiguration](
+        ConfigReaderFailures(new ConfigReaderFailure {
+          override def description: String = "Key not found: 'auth'"
+
+          override def location: Option[ConfigValueLocation] = ConfigValueLocation(config.root().origin())
+        })
+      )
+    }
+
     FunctorK[StorClient].mapK {
-      new DefaultStorClient(conf.uri, conf.auth, httpClient)
+      new DefaultStorClient(conf.uri, auth, httpClient)
+    }(implicitly[FromTask[F]])
+  }
+
+  def readingFromConfig[F[_]: FromTask](config: Config)(implicit sch: Scheduler): ReadingStorClient[F] = {
+    val conf = pureconfig.loadConfigOrThrow[StorClientConfiguration](config.withFallback(DefaultConfig))
+    val httpClient: Client[Task] = Await.result(Http1Client[Task](conf.toBlazeConfig.copy(executionContext = sch)).runAsync, Duration.Inf)
+
+    // will not be used
+    val auth = BasicAuth("", "")
+
+    FunctorK[StorClient].mapK {
+      new DefaultStorClient(conf.uri, auth, httpClient)
     }(implicitly[FromTask[F]])
   }
 }
 
 private case class StorClientConfiguration(uri: Uri,
-                                           auth: BasicAuth,
+                                           auth: Option[BasicAuth],
                                            requestTimeout: Duration,
                                            socketTimeout: Duration,
                                            responseHeaderTimeout: Duration,
